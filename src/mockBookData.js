d49092799c53e393847b1cba8b803909050cee33
Th1nkK1D <witheep@gmail.com>
2020-02-23 21:36:17 +0700

    Add book mock data

---
+++ src/mockBookData.js
@@ -0,0 +1,209 @@
+const bookData = [
+  {
+    "kind": "books#volume",
+    "id": "fU9vuQEACAAJ",
+    "etag": "3vxdHZmHDng",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/fU9vuQEACAAJ",
+    "volumeInfo": {
+      "title": "Company of One",
+      "subtitle": "Why Staying Small Is the Next Big Thing for Business",
+      "authors": [
+        "Paul Jarvis"
+      ],
+      "publisher": "Portfolio",
+      "publishedDate": "2019-01-08",
+      "description": "What if the real key to a richer and more fulfilling career was not to create and scale up a new business, but rather, to be able to work for yourself, determine your own hours and become a (highly profitable) and sustainable company of one? Suppose the better-and smarter-solution is simply to remain small? Company of One is a refreshing new approach centered on staying small and avoiding growth, for any size of business. Not as a freelancer who only gets paid on a per piece basis, and not as an entrepreneurial start-up that wants to scale up as soon as possible, but as a small business that is deliberately committed to staying that way. By staying small, you can have freedom to pursue more meaningful pleasures in life, and avoid the headaches that result from dealing with employees, long meetings, or worrying about expansion. Company of One introduces this unique business strategy and explains how to make it work for you, including how to generate cash flow on an ongoing basis. Paul Jarvis left the corporate world when he realized that working in a high-pressure, high-profile world was not his idea of success. Instead, he now works for himself out of his home, and lives a much more rewarding and productive life. He no longer has to contend with an environment that constantly demands more productivity, more output and more growth. In Company of One, Jarvis explains how you can do the same, including planning to set up, determining desired revenues and keeping clients happy, and of course, doing all this on your own.",
+      "industryIdentifiers": [
+        {
+          "type": "ISBN_10",
+          "identifier": "0241380227"
+        },
+        {
+          "type": "ISBN_13",
+          "identifier": "9780241380222"
+        }
+      ],
+      "readingModes": {
+        "text": false,
+        "image": false
+      },
+      "pageCount": 272,
+      "printType": "BOOK",
+      "averageRating": 5.0,
+      "ratingsCount": 1,
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": false,
+      "contentVersion": "preview-1.0.0",
+      "panelizationSummary": {
+        "containsEpubBubbles": false,
+        "containsImageBubbles": false
+      },
+      "imageLinks": {
+        "smallThumbnail": "http://books.google.com/books/content?id=fU9vuQEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+        "thumbnail": "http://books.google.com/books/content?id=fU9vuQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+      },
+      "language": "en",
+      "previewLink": "http://books.google.co.th/books?id=fU9vuQEACAAJ&dq=9780241380222&hl=&cd=1&source=gbs_api",
+      "infoLink": "http://books.google.co.th/books?id=fU9vuQEACAAJ&dq=9780241380222&hl=&source=gbs_api",
+      "canonicalVolumeLink": "https://books.google.com/books/about/Company_of_One.html?hl=&id=fU9vuQEACAAJ"
+    },
+    "saleInfo": {
+      "country": "TH",
+      "saleability": "NOT_FOR_SALE",
+      "isEbook": false
+    },
+    "accessInfo": {
+      "country": "TH",
+      "viewability": "NO_PAGES",
+      "embeddable": false,
+      "publicDomain": false,
+      "textToSpeechPermission": "ALLOWED",
+      "epub": {
+        "isAvailable": false
+      },
+      "pdf": {
+        "isAvailable": false
+      },
+      "webReaderLink": "http://play.google.com/books/reader?id=fU9vuQEACAAJ&hl=&printsec=frontcover&source=gbs_api",
+      "accessViewStatus": "NONE",
+      "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+      "textSnippet": "In Company of One, Jarvis explains how you can do the same, including planning to set up, determining desired revenues and keeping clients happy, and of course, doing all this on your own."
+    }
+  },
+  {
+    "kind": "books#volume",
+    "id": "2tVzCQAAQBAJ",
+    "etag": "Cu++ZS+65KE",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/2tVzCQAAQBAJ",
+    "volumeInfo": {
+      "title": "เทรดแบบเซียนหุ้น ให้ได้กำไรขั้นเทพ",
+      "subtitle": "Trade Like A Stock Market Wizard",
+      "authors": [
+        "มาร์ค มิเนอร์วินี"
+      ],
+      "publisher": "Nation Books",
+      "publishedDate": "2015",
+      "description": "เทรดแบบเซียนหุ้น ให้ได้กำไรขั้นเทพ มาร์ค มิเนอร์วินี ผู้ชนะรางวัล US Investing Champion เปิดเผยระบบการเทรดของเขาเป็นครั้งแรกอย่างที่ไม่เคยบอกที่ไหนมาก่อน เป็นระบบที่ใช้เวลาทดสอบและผ่านการพิสูจน์มาแล้ว และทำให้เขาได้ผลตอบแทนเป็นตัวเลขเปอร์เซ็นต์ 3 หลักเป็นเวลา 5 ปีติดต่อกัน เฉลี่ยประมาณ 220% ต่อปีสำหรับผลตอบแทนรวมทบต้น 33,500% ในหนังสือ เทรดแบบเซียนหุ้น ให้ได้กำไรขั้นเทพ เล่มนี้ มิเนอร์วินีเปิดเผยวิธีการทางตลาดหุ้นที่เรียกว่า SEPA ซึ่งเขาได้จดทะเบียนเป็นเครื่องหมายทางการค้าไว้แล้ว SEPA เป็นวิธีการที่ให้ผลตอบแทนมากกว่าปกติที่ทำได้ในทุกตลาด ด้วยการผสมผสานการบริหารความเสี่ยงที่มีความรอบคอบ การวิเคราะห์ตัวเอง และความพยายามมุ่งมั่นเอาไว้ด้วยกัน เขาได้อธิบายไว้อย่างละเอียดว่า จะเลือกจุดขาเข้าที่แม่นยำและรักษาเงินต้นได้อย่างไร เพื่อให้ได้ผลตอบแทนเป็นตัวเลขเปอร์เซ็นต์ 3 หลักอย่างต่อเนื่องสม่ำเสมอ ไม่ว่าคุณจะเพิ่งเริ่มเข้ามาในตลาด หรือคุณเป็นมือโปรที่ช่ำชองแล้วก็ตาม มิเนอร์วินีจะแสดงให้เห็นว่า ต้องทำอย่างไรจึงจะได้ผลกำไรขั้นเทพ! คุณจะได้ความรู้อันมีค่าจากบทเรียน ความเป็นจริงของการเทรด และกลยุทธ์เฉพาะตัวที่เขาได้นำมาบอกเล่าไว้ในหนังสือเล่มนี้ ทั้งหมดนี้มาจากประสบการณ์ตลอด 30 ปีในฐานะนักเทรดหุ้นที่ประสบความสำเร็จมากที่สุด เทรดแบบเซียนหุ้น ให้ได้กำไรขั้นเทพ จะบอกคุณว่า • จะหาหุ้นตัวเก่งสุดได้อย่างไรก่อนหุ้นทำกำไรก้อนโต • จะเลี่ยงความผิดพลาดราคาแพงที่นักลงทุนส่วนใหญ่ทำได้อย่างไร • จะจัดการผลขาดทุนและรักษาผลกำไรได้อย่างไร • จะหลีกหนีสถานการณ์ที่มีความเสี่ยงสูงได้อย่างไร • จะซื้อและขายเมื่อไหร่ให้ถูกต้องแม่นยำ • จะซื้อหุ้นที่เสนอขายให้นักลงทุนครั้งแรกได้อย่างไร • ทำไมการประเมินแบบดั้งเดิมใช้ไม่ได้ผลกับหุ้นดาวรุ่งโตเร็ว • ตัวอย่างการเทรดส่วนตัวของมิเนอร์วินี พร้อมความเห็น ตัวอย่างแผนภาพมากกว่า 160 ตัวอย่าง และกรณีศีกษาจำนวนมากที่พิสูจน์หลักการของมิเนอร์วินีแล้วว่ามีประสิทธิภาพอย่างเห็นได้ชัด หนังสือ เทรดแบบเซียนหุ้น ให้ได้กำไรขั้นเทพ ทำให้คุณมีระบบการลงทุนหุ้นมาอยู่ในมือ เป็นระบบการลงทุนที่มีประสิทธิภาพมากที่สุดในโลกวิธีหนึ่ง ซึ่งยังคงเป็นความลับจนถึงบัดนี้ มาร์ค มิเนอร์วินี นักเทรดที่ประสบความสำเร็จมากที่สุดคนหนึ่งของประเทศสหรัฐอเมริกา เคยเป็นผู้ช่ำชองในตลาดหุ้นวอลล์สตรีทมาเกือบ 30 ปี เขาก่อตั้ง Minervini Private Access ซึ่งเป็นโครงสร้างที่ให้บริการการเทรดหุ้นในวิธีการ SEPA แบบเรียลไทม์สำหรับสมาชิกออนไลน์เท่านั้น ละยังจัดอบรมโปรแกรม Master Trader Program ที่ผู้เข้าร่วมได้ประสบการณ์ตรงจากการศึกษาหลักการของเขา",
+      "industryIdentifiers": [
+        {
+          "type": "ISBN_13",
+          "identifier": "9786165156189"
+        },
+        {
+          "type": "ISBN_10",
+          "identifier": "6165156185"
+        }
+      ],
+      "readingModes": {
+        "text": false,
+        "image": true
+      },
+      "pageCount": 390,
+      "printType": "BOOK",
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": true,
+      "contentVersion": "0.55.0.0.preview.1",
+      "panelizationSummary": {
+        "containsEpubBubbles": false,
+        "containsImageBubbles": false
+      },
+      "imageLinks": {
+        "smallThumbnail": "http://books.google.com/books/content?id=2tVzCQAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        "thumbnail": "http://books.google.com/books/content?id=2tVzCQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      },
+      "language": "th",
+      "previewLink": "http://books.google.co.th/books?id=2tVzCQAAQBAJ&printsec=frontcover&dq=9780241380222&hl=&cd=2&source=gbs_api",
+      "infoLink": "http://books.google.co.th/books?id=2tVzCQAAQBAJ&dq=9780241380222&hl=&source=gbs_api",
+      "canonicalVolumeLink": "https://books.google.com/books/about/%E0%B9%80%E0%B8%97%E0%B8%A3%E0%B8%94%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B9%80%E0%B8%8B%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%AB.html?hl=&id=2tVzCQAAQBAJ"
+    },
+    "saleInfo": {
+      "country": "TH",
+      "saleability": "NOT_FOR_SALE",
+      "isEbook": false
+    },
+    "accessInfo": {
+      "country": "TH",
+      "viewability": "PARTIAL",
+      "embeddable": true,
+      "publicDomain": false,
+      "textToSpeechPermission": "ALLOWED",
+      "epub": {
+        "isAvailable": false
+      },
+      "pdf": {
+        "isAvailable": true,
+        "acsTokenLink": "http://books.google.co.th/books/download/%E0%B9%80%E0%B8%97%E0%B8%A3%E0%B8%94%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B9%80%E0%B8%8B%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%AB-sample-pdf.acsm?id=2tVzCQAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+      },
+      "webReaderLink": "http://play.google.com/books/reader?id=2tVzCQAAQBAJ&hl=&printsec=frontcover&source=gbs_api",
+      "accessViewStatus": "SAMPLE",
+      "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+      "textSnippet": "เทรดแบบเซียนหุ้น ให้ได้กำไรขั้นเทพ มาร์ค มิเนอร์วินี ผู้ชนะรางวัล US Investing Champion ..."
+    }
+  },
+  {
+    "kind": "books#volume",
+    "id": "Vk4YAAAACAAJ",
+    "etag": "2CW+n2E24qk",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/Vk4YAAAACAAJ",
+    "volumeInfo": {
+      "title": "จุดจบแห่งจินตนาการ",
+      "authors": [
+        "Arundhati Roy"
+      ],
+      "publishedDate": "2005",
+      "industryIdentifiers": [
+        {
+          "type": "ISBN_10",
+          "identifier": "9749314298"
+        },
+        {
+          "type": "ISBN_13",
+          "identifier": "9789749314296"
+        }
+      ],
+      "readingModes": {
+        "text": false,
+        "image": false
+      },
+      "pageCount": 149,
+      "printType": "BOOK",
+      "categories": [
+        "India"
+      ],
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": false,
+      "contentVersion": "preview-1.0.0",
+      "language": "th",
+      "previewLink": "http://books.google.co.th/books?id=Vk4YAAAACAAJ&dq=9780241380222&hl=&cd=3&source=gbs_api",
+      "infoLink": "http://books.google.co.th/books?id=Vk4YAAAACAAJ&dq=9780241380222&hl=&source=gbs_api",
+      "canonicalVolumeLink": "https://books.google.com/books/about/%E0%B8%88%E0%B8%B8%E0%B8%94%E0%B8%88%E0%B8%9A%E0%B9%81%E0%B8%AB%E0%B9%88%E0%B8%87%E0%B8%88%E0%B8%B4%E0%B8%99%E0%B8%95.html?hl=&id=Vk4YAAAACAAJ"
+    },
+    "saleInfo": {
+      "country": "TH",
+      "saleability": "NOT_FOR_SALE",
+      "isEbook": false
+    },
+    "accessInfo": {
+      "country": "TH",
+      "viewability": "NO_PAGES",
+      "embeddable": false,
+      "publicDomain": false,
+      "textToSpeechPermission": "ALLOWED",
+      "epub": {
+        "isAvailable": false
+      },
+      "pdf": {
+        "isAvailable": false
+      },
+      "webReaderLink": "http://play.google.com/books/reader?id=Vk4YAAAACAAJ&hl=&printsec=frontcover&source=gbs_api",
+      "accessViewStatus": "NONE",
+      "quoteSharingAllowed": false
+    }
+  }
+];
+
+export default bookData;
