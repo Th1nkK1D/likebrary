74177616c24ddedd7dff38ed2ad5a53257243359
Th1nkK1D <witheep@gmail.com>
2020-02-23 21:36:11 +0700

    Cleanup main page

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script lang="typescript">
-	export let name: string;
+	import bookData from './mockBookdata';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>LK's Library</h1>
+	<p>Feel free to morrow one because book is meant for sharing.</p>
 </main>
 
 <style lang="scss">
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
-
-		h1 {
-			color: #ff3e00;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		padding: 0 4rem;
 	}
 </style>
